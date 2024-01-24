12cd194d56265ffddcf24e741e508c9732783378
MrYogesh0709 <yogeshvanzara98@gmail.com>
2024-01-24 23:19:19 +0530

    side by side view added

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,17 +1,25 @@
 import {
+  Avatar,
   Box,
   Button,
   CircularProgress,
   Container,
   Divider,
+  List,
+  ListItem,
+  ListItemText,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
-import useGetUsers from "../hook/getUsers";
+import useGetUsers, { UserType } from "../hook/getUsers";
 import User from "./User";
 import { useState } from "react";
 
 function Home() {
   const [users, loading, isError, errorMessage] = useGetUsers();
+  const theme = useTheme();
+  const [selectedUser, setSelectedUser] = useState<UserType | null>(null);
 
   const itemsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
@@ -23,7 +31,16 @@ function Home() {
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
+    setSelectedUser(null);
   };
+
+  const handleUserClick = (user: UserType) => {
+    setSelectedUser(user);
+  };
+  const formattedCreatedAt =
+    selectedUser && new Date(selectedUser.createdAt).toLocaleString();
+
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
 
   if (loading) {
     return (
@@ -68,12 +85,92 @@ function Home() {
             flexDirection: "column",
           }}
         >
-          <Typography variant="h3">All User</Typography>
+          <Typography variant="h3" sx={{ textAlign: "center" }}>
+            All User
+          </Typography>
           <Divider aria-hidden="true" />
-          {/* users */}
-          {displayedUsers.map((user, i) => (
-            <User user={user} key={i} />
-          ))}
+          {/* all users */}
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2,
+              width: "100%",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                width: `${selectedUser && isLargeScreen ? "50%" : "100%"}`,
+              }}
+            >
+              {displayedUsers.map((user: UserType, i: number) => (
+                <User user={user} key={i} handleClick={handleUserClick} />
+              ))}
+            </Box>
+            {isLargeScreen && selectedUser && (
+              <Divider orientation="vertical" flexItem />
+            )}
+            {isLargeScreen && selectedUser && (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: 2,
+                  width: "50%",
+                }}
+              >
+                <Box p={2} width="100%">
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      mb: 1,
+                    }}
+                  >
+                    <Avatar
+                      src={selectedUser.avatar}
+                      alt={`${selectedUser.profile.firstName} Avatar`}
+                    />
+                    <Typography variant="h5">
+                      {selectedUser.profile.firstName}{" "}
+                      {selectedUser.profile.lastName}
+                    </Typography>
+                  </Box>
+
+                  <Divider />
+                  <List>
+                    <ListItem>
+                      <ListItemText
+                        primary={`Username: ${selectedUser.profile.username}`}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText
+                        primary={`Email: ${selectedUser.profile.email}`}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText
+                        primary={`Job Title: ${selectedUser.jobTitle}`}
+                      />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary={`Bio: ${selectedUser.Bio}`} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText
+                        primary={`Created At: ${formattedCreatedAt}`}
+                      />
+                    </ListItem>
+                  </List>
+                </Box>
+              </Box>
+            )}
+          </Box>
           {/* pagination */}
           <Box
             sx={{
--- src/Home/User.tsx
+++ src/Home/User.tsx
@@ -1,11 +1,25 @@
-import { Avatar, Box, Button, Divider, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { UserType } from "../hook/getUsers";
 import UserDetailsDrawer from "./UserDetailsDrawer";
 import { useState } from "react";
 
-const User = ({ user }: { user: UserType }) => {
+const User = ({
+  user,
+  handleClick,
+}: {
+  user: UserType;
+  handleClick: (user: UserType) => void;
+}) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-
+  const theme = useTheme();
   const handleDrawerOpen = () => {
     setDrawerOpen(true);
   };
@@ -13,13 +27,24 @@ const User = ({ user }: { user: UserType }) => {
   const handleDrawerClose = () => {
     setDrawerOpen(false);
   };
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const handleClickWithScroll = (user: UserType) => {
+    handleClick(user);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <>
-      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+        }}
+      >
         <Avatar
           src={user.avatar}
           alt={user.profile.firstName.charAt(0)}
+          sx={{ mt: 2 }}
         ></Avatar>
         <Box sx={{ display: "flex", flexDirection: "column" }}>
           <Typography variant="h5" color="text.primary">
@@ -35,7 +60,10 @@ const User = ({ user }: { user: UserType }) => {
               color="inherit"
               sx={{ p: 0, textTransform: "none" }}
               onClick={() => {
-                handleDrawerOpen();
+                {
+                  isSmallScreen && handleDrawerOpen();
+                }
+                handleClickWithScroll(user);
               }}
             >
               Other Info...
@@ -44,11 +72,13 @@ const User = ({ user }: { user: UserType }) => {
         </Box>
       </Box>
       <Divider aria-hidden="true" />
-      <UserDetailsDrawer
-        user={user}
-        open={drawerOpen}
-        onClose={handleDrawerClose}
-      />
+      {isSmallScreen && (
+        <UserDetailsDrawer
+          user={user}
+          open={drawerOpen}
+          onClose={handleDrawerClose}
+        />
+      )}
     </>
   );
 };
